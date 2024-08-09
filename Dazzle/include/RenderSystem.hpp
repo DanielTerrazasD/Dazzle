0163304f8c0ef8c9d3fc81da8c1572e5b6be4b90
Daniel Terrazas <daniel.terrazas.delgado@gmail.com>
2024-08-09 00:23:57 -0600

    Adding a shader manager and a couple of shader and program builders

--- Dazzle/include/RenderSystem.hpp
+++ Dazzle/include/RenderSystem.hpp
@@ -8,51 +8,114 @@
 
 #include "DazzleEngine.hpp"
 
-namespace DazzleEngine
+namespace Dazzle
 {
     namespace RenderSystem
     {
-        namespace GLSL
-        {
-        }
-
-        namespace GL
-        {
-            class Shader
-            {
-            public:
-                Shader();
-                GLuint& CreateShader(const GLenum type, const std::string& source);
-                ~Shader();
-            
-            private:
-                GLuint mShader;
-            };
-
-            class Program
-            {
-                Program();
-                void AttachShader(const GLuint shader);
-                GLuint GetProgramHandle();
-                ~Program();
-
-            private:
-                GLuint mProgram;
-            };
-            
-            // class ProgramManager {}; // Keeps track of all the created programs. Delete them when the application finishes
-
-            void SetupDebugMessageCallback();
-            void DebugOutput(   GLenum source,
-                                GLenum type,
-                                GLuint id,
-                                GLenum severity,
-                                GLsizei length,
-                                const GLchar *message,
-                                const void *userParam);
-
-            // class ShaderManager {};
-        }
     }
+
+    namespace GL
+    {
+        class ShaderId
+        {
+        public:
+            ShaderId();
+            ~ShaderId();
+            GLuint GetHandle() const;
+            bool IsValid() const;
+        private:
+            friend class ShaderBuilder;
+            GLuint mHandle;
+        };
+
+        class ShaderBuilder
+        {
+        public:
+            ShaderBuilder();
+            ~ShaderBuilder();
+            ShaderId Create(const GLenum& type, const std::string& source);
+        };
+
+        class ProgramId
+        {
+        public:
+            ProgramId();
+            ~ProgramId();
+            GLuint GetHandle() const;
+            bool IsValid() const;
+        private:
+            friend class ProgramBuilder;
+            GLuint mHandle;
+        };
+
+        class ProgramBuilder
+        {
+        public:
+            ProgramBuilder();
+            ~ProgramBuilder();
+            ProgramId Create();
+            void AttachShader(const ShaderId& shader, const ProgramId& program);
+            void Link(const ProgramId& program);
+        };
+        
+        void SetupDebugMessageCallback();
+        void DebugMessageCallback(   GLenum source,
+                            GLenum type,
+                            GLuint id,
+                            GLenum severity,
+                            GLsizei length,
+                            const GLchar *message,
+                            const void *userParam);
+
+    }
+
+    enum class ShaderType
+    {
+        VERTEX_SHADER =             1 << 0,
+        TESS_CONTROL_SHADER =       1 << 1,
+        TESS_EVALUATION_SHADER =    1 << 2,
+        GEOMETRY_SHADER =           1 << 3,
+        FRAGMENT_SHADER =           1 << 4,
+        COMPUTE_SHADER =            1 << 5
+    };
+
+    inline ShaderType operator|(ShaderType lhs, ShaderType rhs)
+    {
+        return static_cast<ShaderType>(
+            static_cast<std::underlying_type<ShaderType>::type>(lhs) |
+            static_cast<std::underlying_type<ShaderType>::type>(rhs));
+    }
+
+    inline ShaderType operator&(ShaderType lhs, ShaderType rhs)
+    {
+        return static_cast<ShaderType>(
+            static_cast<std::underlying_type<ShaderType>::type>(lhs) &
+            static_cast<std::underlying_type<ShaderType>::type>(rhs));
+    }
+
+    inline bool hasType(ShaderType bitField, ShaderType type)
+    {
+        return (static_cast<std::underlying_type<ShaderType>::type>(bitField) &
+                static_cast<std::underlying_type<ShaderType>::type>(type)) != 0;
+    }
+
+    struct ShaderSources
+    {
+        std::string mVSSC;  // Vertex Shader Source Code
+        std::string mTCSSC; // Tessellation Control Shader Source Code
+        std::string mTESSC; // Tessellation Evaluation Shader Source Code
+        std::string mGSSC;  // Geometry Shader Source Code
+        std::string mFSSC;  // Fragment Shader Source Code
+        std::string mCSSC;  // Compute Shader Source Code
+    };
+
+    class ShaderManager
+    {
+    public:
+        void LoadShader(const ShaderSources& sources);
+        void UseShader(/*Select by shaderId or shaderName*/);
+    private:
+        // std::unordered_map<> mShaders;
+    };
 }
 #endif // _RENDER_SYSTEM_HPP_
--- Dazzle/source/RenderSystem.cpp
+++ Dazzle/source/RenderSystem.cpp
@@ -1,14 +1,105 @@
 #include <iostream>
+#include <stdexcept>
 
 #include "RenderSystem.hpp"
 
-DazzleEngine::RenderSystem::GL::Shader::Shader() : mShader(0)
+void Dazzle::GL::SetupDebugMessageCallback()
 {
+    GLint contextFlags;
+    glGetIntegerv(GL_CONTEXT_FLAGS, &contextFlags);
 
+    if (contextFlags & GL_CONTEXT_FLAG_DEBUG_BIT)
+    {
+        std::cout << "Debug Context Created\n";
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        glDebugMessageCallback(DebugMessageCallback, nullptr);
+        // Enable all message's sources, types and severities:
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+
+        // Filter out specific id:
+        GLuint filterOutId = 131185;
+        glDebugMessageControl(GL_DEBUG_SOURCE_API, GL_DEBUG_TYPE_OTHER, GL_DONT_CARE, 1, &filterOutId, GL_FALSE);
+    }
 }
 
-GLuint& DazzleEngine::RenderSystem::GL::Shader::CreateShader(const GLenum type, const std::string& source)
+void Dazzle::GL::DebugMessageCallback(  GLenum source,
+                                        GLenum type,
+                                        GLuint id,
+                                        GLenum severity,
+                                        GLsizei length,
+                                        const GLchar* message,
+                                        const void* userParam)
 {
+    std::cout << "----------------------------------------\n";
+    std::cout << "|Debug Callback                        |\n";
+    std::cout << "----------------------------------------\n";
+    std::cout << "ID: " << id << '\n';
+
+    std::cout << "Message Source: ";
+    switch (source)
+    {
+        case GL_DEBUG_SOURCE_API:             std::cout << "GL_DEBUG_SOURCE_API\n";             break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "GL_DEBUG_SOURCE_WINDOW_SYSTEM\n";   break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "GL_DEBUG_SOURCE_SHADER_COMPILER\n"; break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "GL_DEBUG_SOURCE_THIRD_PARTY\n";     break;
+        case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "GL_DEBUG_SOURCE_APPLICATION\n";     break;
+        case GL_DEBUG_SOURCE_OTHER:           std::cout << "GL_DEBUG_SOURCE_OTHER\n";           break;
+    }
+
+    std::cout << "Error Type: ";
+    switch (type)
+    {
+        case GL_DEBUG_TYPE_ERROR:               std::cout << "GL_DEBUG_TYPE_ERROR\n";               break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR\n"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR\n";  break;
+        case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "GL_DEBUG_TYPE_PORTABILITY\n";         break;
+        case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "GL_DEBUG_TYPE_PERFORMANCE\n";         break;
+        case GL_DEBUG_TYPE_MARKER:              std::cout << "GL_DEBUG_TYPE_MARKER\n";              break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "GL_DEBUG_TYPE_PUSH_GROUP\n";          break;
+        case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "GL_DEBUG_TYPE_POP_GROUP\n";           break;
+        case GL_DEBUG_TYPE_OTHER:               std::cout << "GL_DEBUG_TYPE_OTHER\n";               break;
+    }
+
+    std::cout << "Severity: ";
+    switch (severity)
+    {
+        case GL_DEBUG_SEVERITY_HIGH:         std::cout << "GL_DEBUG_SEVERITY_HIGH\n";           break;
+        case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "GL_DEBUG_SEVERITY_MEDIUM\n";         break;
+        case GL_DEBUG_SEVERITY_LOW:          std::cout << "GL_DEBUG_SEVERITY_LOW\n";            break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "GL_DEBUG_SEVERITY_NOTIFICATION\n";   break;
+    }
+
+    std::cout << "\nMessage:\n" << message << "\n";
+    // Optionally, throw an exception or assert:
+    throw std::runtime_error(message);
+}
+
+Dazzle::GL::ShaderId::ShaderId() : mHandle(0) {}
+
+Dazzle::GL::ShaderId::~ShaderId()
+{
+    glDeleteShader(mHandle);
+}
+
+GLuint Dazzle::GL::ShaderId::GetHandle() const
+{
+    return mHandle;
+}
+
+bool Dazzle::GL::ShaderId::IsValid() const
+{
+    return mHandle > 0;
+}
+
+Dazzle::GL::ShaderBuilder::ShaderBuilder() {}
+
+Dazzle::GL::ShaderBuilder::~ShaderBuilder() {}
+
+Dazzle::GL::ShaderId Dazzle::GL::ShaderBuilder::Create(const GLenum& type, const std::string& source)
+{
+    ShaderId s;
+
     switch (type)
     {
     case GL_VERTEX_SHADER:
@@ -21,137 +112,136 @@ GLuint& DazzleEngine::RenderSystem::GL::Shader::CreateShader(const GLenum type, 
 
     default:
         // Error: GL_INVALID_ENUM
-        mShader = 0;
-        return mShader;
+        s = ShaderId();
+        return s;
     }
 
-    mShader = glCreateShader(type);
+    s.mHandle = glCreateShader(type);
     const GLchar* sourcePtr = source.c_str();
     GLint sourceLength = static_cast<GLint>(source.size());
-    glShaderSource(mShader, 1, &sourcePtr, &sourceLength);
-    glCompileShader(mShader);
+    glShaderSource(s.mHandle, 1, &sourcePtr, &sourceLength);
+    glCompileShader(s.mHandle);
 
     GLint compileStatus;
-    glGetShaderiv(mShader, GL_COMPILE_STATUS, &compileStatus);
+    glGetShaderiv(s.mHandle, GL_COMPILE_STATUS, &compileStatus);
     if (compileStatus != GL_TRUE)
     {
         GLint logLength;
-        glGetShaderiv(mShader, GL_INFO_LOG_LENGTH, &logLength);
+        glGetShaderiv(s.mHandle, GL_INFO_LOG_LENGTH, &logLength);
         std::string log(logLength, '\0');
-        glGetShaderInfoLog(mShader, logLength, &logLength, &log[0]);
+        glGetShaderInfoLog(s.mHandle, logLength, &logLength, &log[0]);
         std::cerr << "Shader compilation failed:\n" << log << '\n';
     }
 
-    return mShader;
+    return s;
 }
 
-DazzleEngine::RenderSystem::GL::Shader::~Shader()
+Dazzle::GL::ProgramId::ProgramId() : mHandle(0) {}
+
+Dazzle::GL::ProgramId::~ProgramId()
 {
-    glDeleteShader(mShader);
+    glDeleteProgram(mHandle);
 }
 
-DazzleEngine::RenderSystem::GL::Program::Program()
+GLuint Dazzle::GL::ProgramId::GetHandle() const
 {
-
+    return mHandle;
 }
 
-void DazzleEngine::RenderSystem::GL::Program::AttachShader(const GLuint shader)
+bool Dazzle::GL::ProgramId::IsValid() const
 {
-    if (glIsShader(shader) != GL_TRUE)
+    return mHandle > 0;
+}
+
+Dazzle::GL::ProgramBuilder::ProgramBuilder() {}
+
+Dazzle::GL::ProgramBuilder::~ProgramBuilder() {}
+
+Dazzle::GL::ProgramId Dazzle::GL::ProgramBuilder::Create()
+{
+    ProgramId p{};
+    p.mHandle = glCreateProgram();
+    return p;
+}
+
+void Dazzle::GL::ProgramBuilder::AttachShader(const ShaderId& shader, const ProgramId& program)
+{
+    if (glIsShader(shader.GetHandle()) != GL_TRUE)
     {
-        std::cerr << "Unable to attach Shader to Program: Invalid Shader object\n";
+        std::cerr << "Unable to attach Shader to Program: Invalid Shader Object\n";
         return;
     }
 
-    glAttachShader(mProgram, shader);
+    glAttachShader(program.GetHandle(), shader.GetHandle());
 }
 
-GLuint DazzleEngine::RenderSystem::GL::Program::GetProgramHandle()
+void Dazzle::GL::ProgramBuilder::Link(const ProgramId& program)
 {
-    glLinkProgram(mProgram);
+    glLinkProgram(program.GetHandle());
 
     GLint linkageStatus;
-    glGetProgramiv(mProgram, GL_LINK_STATUS, &linkageStatus);
+    glGetProgramiv(program.GetHandle(), GL_LINK_STATUS, &linkageStatus);
     if (linkageStatus != GL_TRUE)
     {
         GLint logLength;
-        glGetProgramiv(mProgram, GL_INFO_LOG_LENGTH, &logLength);
+        glGetProgramiv(program.GetHandle(), GL_INFO_LOG_LENGTH, &logLength);
         std::string log(logLength, '\0');
-        glGetProgramInfoLog(mProgram, logLength, &logLength, &log[0]);
+        glGetProgramInfoLog(program.GetHandle(), logLength, &logLength, &log[0]);
         std::cerr << "Program linkage failed:\n" << log << '\n';
     }
-
-    return mProgram;
 }
 
-DazzleEngine::RenderSystem::GL::Program::~Program()
+void Dazzle::ShaderManager::LoadShader(const ShaderSources& sources)
 {
-    
-}
+    GL::ShaderId vs{};  // Vertex shader
+    GL::ShaderId tcs{}; // Tessellation control shader
+    GL::ShaderId tes{}; // Tessellation evalation shader
+    GL::ShaderId gs{};  // Geometry shader
+    GL::ShaderId fs{};  // Fragment shader
+    GL::ShaderId cs{};  // Fragment shader
 
-void DazzleEngine::RenderSystem::GL::SetupDebugMessageCallback()
-{
-    GLint contextFlags;
-    glGetIntegerv(GL_CONTEXT_FLAGS, &contextFlags);
+    GL::ShaderBuilder glShaderBuilder{};
 
-    if (contextFlags & GL_CONTEXT_FLAG_DEBUG_BIT)
-    {
-        std::cout << "Debug Context Created\n";
-        glEnable(GL_DEBUG_OUTPUT);
-        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-        glDebugMessageCallback(DebugOutput, nullptr);
-        glDebugMessageControl(  GL_DONT_CARE,
-                                GL_DONT_CARE,
-                                GL_DONT_CARE,
-                                0,
-                                nullptr,
-                                GL_TRUE);
-    }
-}
+    if (!sources.mVSSC.empty())
+        vs = glShaderBuilder.Create(GL_VERTEX_SHADER, sources.mVSSC);
 
-void DazzleEngine::RenderSystem::GL::DebugOutput(   GLenum source,
-                                                    GLenum type,
-                                                    GLuint id,
-                                                    GLenum severity,
-                                                    GLsizei length,
-                                                    const GLchar* message,
-                                                    const void* userParam)
-{
-    std::cout << "Debug Callback\n";
-    std::cout << "ID: %d\n";
-    std::cout << "Message:\n %s\n";
+    if (!sources.mTCSSC.empty())
+        tcs = glShaderBuilder.Create(GL_TESS_CONTROL_SHADER, sources.mTCSSC);
 
-    std::cout << "Message Source: ";
-    switch (source)
-    {
-        case GL_DEBUG_SOURCE_API:             std::cout << "GL_DEBUG_SOURCE_API\n"; break;
-        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "GL_DEBUG_SOURCE_WINDOW_SYSTEM\n"; break;
-        case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "GL_DEBUG_SOURCE_SHADER_COMPILER\n"; break;
-        case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "GL_DEBUG_SOURCE_THIRD_PARTY\n"; break;
-        case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "GL_DEBUG_SOURCE_APPLICATION\n"; break;
-        case GL_DEBUG_SOURCE_OTHER:           std::cout << "GL_DEBUG_SOURCE_OTHER\n"; break;
-    }
+    if (!sources.mTESSC.empty())
+        tes = glShaderBuilder.Create(GL_TESS_EVALUATION_SHADER, sources.mTESSC);
 
-    std::cout << "Error Type: ";
-    switch (type)
-    {
-        case GL_DEBUG_TYPE_ERROR:               std::cout << "GL_DEBUG_TYPE_ERROR\n"; break;
-        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR\n"; break;
-        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR\n"; break;
-        case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "GL_DEBUG_TYPE_PORTABILITY\n"; break;
-        case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "GL_DEBUG_TYPE_PERFORMANCE\n"; break;
-        case GL_DEBUG_TYPE_MARKER:              std::cout << "GL_DEBUG_TYPE_MARKER\n"; break;
-        case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "GL_DEBUG_TYPE_PUSH_GROUP\n"; break;
-        case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "GL_DEBUG_TYPE_POP_GROUP\n"; break;
-        case GL_DEBUG_TYPE_OTHER:               std::cout << "GL_DEBUG_TYPE_OTHER\n"; break;
-    }
+    if (!sources.mGSSC.empty())
+        gs = glShaderBuilder.Create(GL_GEOMETRY_SHADER, sources.mGSSC);
 
-    std::cout << "Severity: ";
-    switch (severity)
-    {
-        case GL_DEBUG_SEVERITY_HIGH:         std::cout << "GL_DEBUG_SEVERITY_HIGH\n"; break;
-        case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "GL_DEBUG_SEVERITY_MEDIUM\n"; break;
-        case GL_DEBUG_SEVERITY_LOW:          std::cout << "GL_DEBUG_SEVERITY_LOW\n"; break;
-        case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "GL_DEBUG_SEVERITY_NOTIFICATION\n"; break;
-    }
+    if (!sources.mFSSC.empty())
+        fs = glShaderBuilder.Create(GL_FRAGMENT_SHADER, sources.mFSSC);
+
+    if (!sources.mCSSC.empty())
+        cs = glShaderBuilder.Create(GL_COMPUTE_SHADER, sources.mCSSC);
+
+    GL::ProgramBuilder glProgramBuilder{};
+    GL::ProgramId p = glProgramBuilder.Create();
+
+    if (vs.IsValid())
+        glProgramBuilder.AttachShader(vs, p);
+
+    if (tcs.IsValid())
+        glProgramBuilder.AttachShader(tcs, p);
+
+    if (tes.IsValid())
+        glProgramBuilder.AttachShader(tes, p);
+
+    if (gs.IsValid())
+        glProgramBuilder.AttachShader(gs, p);
+
+    if (fs.IsValid())
+        glProgramBuilder.AttachShader(fs, p);
+
+    if (cs.IsValid())
+        glProgramBuilder.AttachShader(cs, p);
+
+    glProgramBuilder.Link(p);
+    // if (p.IsValid())
+        // Insert it into the mShaders map.
 }
