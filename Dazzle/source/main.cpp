90158b45753a72307c81c55d7ce1cc8e0cd3902e
Daniel Terrazas <daniel.terrazas.delgado@gmail.com>
2024-08-09 00:24:42 -0600

    Setting up the glfw hint to create a debug context

--- Dazzle/source/main.cpp
+++ Dazzle/source/main.cpp
@@ -11,6 +11,8 @@
 #include <iostream>
 #include <stdio.h>
 
+#include "RenderSystem.hpp"
+
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
@@ -31,6 +33,8 @@ int main(int, char**)
         return 1;
 
     const char* glsl_version = "#version 460";
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 
@@ -77,6 +81,9 @@ int main(int, char**)
     bool show_demo_window = true;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+    Dazzle::GL::SetupDebugMessageCallback();
+
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
@@ -89,6 +96,28 @@ int main(int, char**)
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
         if (show_demo_window)
             ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
 
         // Rendering
         ImGui::Render();
